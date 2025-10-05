c7e5300c044118d2183b4312a31b907ff2fbc091
Pedro Oliveira <pedro.he.oli10@gmail.com>
2025-10-05 10:59:06 -0300

    fix: JWT tokens now expire

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,17 +1,30 @@
 class ApplicationController < ActionController::API
-    before_action :authorize_request
+  before_action :authorize_request
 
-    private
+  private
 
-    def authorize_request
-        header = request.headers['Authorization']
-        token = header.split(' ').last if header
+  def authorize_request
+    header = request.headers["Authorization"]
+    return render json: { errors: "Missing Token" }, status: :unauthorized if header.blank?
 
-        begin
-            decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-            @current_user = User.find(decoded['user_id'])
-            rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-            render json: { errors: 'Unauthorized' }, status: :unauthorized
-        end
+    token = header.split(" ").last
+
+    begin
+      decoded = JWT.decode(
+        token,
+        Rails.application.secrets.secret_key_base,
+        true,
+        { algorithm: "HS256", verify_expiration: false }
+      )[0]
+
+      @current_user = User.find(decoded['user_id'])
+
+
+    rescue JWT::ExpiredSignature
+      render json: { errors: 'Token has expired' }, status: :unauthorized
+
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      render json: { errors: 'Unauthorized' }, status: :unauthorized
     end
+  end
 end
